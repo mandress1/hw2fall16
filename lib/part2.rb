5be5d050d5f2c6271b61c8577b58a6c30dbe0001
Matt Andress <matthew-andress@uiowa.edu>
2016-09-13 11:45:53 -0500

    part 2 a is done. needs more thorough testing

--- lib/part2.rb
+++ lib/part2.rb
@@ -3,7 +3,41 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
-  # YOUR CODE HERE
+
+  p1Choice = game[0][1].downcase
+  p2Choice = game[1][1].downcase
+
+  raise NoSuchStrategyError unless p1Choice == "r" or p1Choice == "p" or p1Choice == "s"
+  raise NoSuchStrategyError unless p2Choice == "r" or p2Choice == "p" or p2Choice == "s"
+
+  if p1Choice == "r"
+    case p2Choice
+    when "r"
+      return game[0]
+    when "p"
+      return game[1]
+    when "s"
+      return game[0]
+    end
+  elsif p1Choice == "p"
+    case p2Choice
+    when "r"
+      return game[0]
+    when "p"
+      return game[0]
+    when "s"
+      return game[1]
+    end
+  else # p1Choice == "s"
+    case p2Choice
+    when "r"
+      return  game[1]
+    when "p"
+      return game[0]
+    when "s"
+      return game[0]
+    end
+  end  
 end
 
 def rps_tournament_winner(tournament)
