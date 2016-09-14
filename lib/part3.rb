b6aa1fd83a408d7f24987146278068751c1b5f29
Matt Andress <matthew-andress@uiowa.edu>
2016-09-14 11:03:21 -0500

    started part 3. need ro use attr_accessor

--- lib/part3.rb
+++ lib/part3.rb
@@ -1,21 +1,27 @@
 class Dessert
 
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
-    # YOUR CODE HERE
+    calories < 200
   end
   
   def delicious?
-    # YOUR CODE HERE
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def prnt
+    puts "name: #{@name}, cal: #{@calories}, flav: #{@flavor}"
   end
   
   def delicious?
