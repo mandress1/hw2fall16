c271d2339e513aed0c2ab596c391cd03da8a090b
Matt Andress <matthew-andress@uiowa.edu>
2016-09-14 11:34:17 -0500

    finished part 3. added checks to make sure arguments are correct types

--- lib/part3.rb
+++ lib/part3.rb
@@ -4,6 +4,8 @@ class Dessert
   attr_accessor :calories
 
   def initialize(name, calories)
+    raise ArgumentError, "Error: name needs to be a string" unless name.is_a?(String)
+    raise ArgumentError, "Error: calories needs to be a number" unless calories.is_a?(Numeric)
     @name = name
     @calories = calories
   end
@@ -23,6 +25,7 @@ class JellyBean < Dessert
 
   def initialize(name, calories, flavor)
     super(name, calories)
+    raise ArgumentError, "Error: flavor needs to be a string" unless flavor.is_a?(String)
     @flavor = flavor
   end
   
