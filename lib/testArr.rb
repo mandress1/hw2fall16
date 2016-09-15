ce0eec540de823460fbae2da016f1af419ecd74f
Matt Andress <matthew-andress@uiowa.edu>
2016-09-14 21:52:00 -0500

    Removed extra files because apparently the autograder is too dumb to ignore them

--- lib/testArr.rb
+++
@@ -1,3 +0,0 @@
-def getTourn
-  pracTourn = [ [ [ ["Matt", "R"], ["Kyle", "P"] ], [ ["Jake", "S"], ["Nick", "R"] ] ], [ [ ["JD", "P"], ["Grant", "S"] ], [ ["Conor", "R"], ["Sean", "P"] ] ] ]
-end
--- tst2.rb
+++
@@ -1,8 +0,0 @@
-require "./lib/part2.rb"
-require "./lib/testArr.rb"
-
-tst = getTourn
-
-puts "Start tourn: "
-
-puts "Winner: #{rps_tournament_winner(tst)}"
