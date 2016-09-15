4ba0b6fb44521ecbebe50f76a4ee2748fe39bece
Matt Andress <matthew-andress@uiowa.edu>
2016-09-14 21:50:33 -0500

    part 5 completed

--- lib/part5.rb
+++ lib/part5.rb
@@ -1,6 +1,22 @@
 class CartesianProduct
   include Enumerable
 
-  # YOUR CODE HERE
+  def initialize(arr1, arr2)
+    @prod_arr = []
+
+    arr1.each do |el_1|
+      arr2.each do |el_2|
+        @prod_arr.push([el_1, el_2])
+      end
+    end
+
+    @prod_arr
+  end
+
+  def each
+    @prod_arr.each do |el|
+      yield el
+    end
+  end
 
 end
